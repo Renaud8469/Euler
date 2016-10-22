02537874219366199106a5a18233f8a8c6f92a81
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-10-22 17:50:28 +0200

    Initial commit

---
+++ PrimeFactors.py
@@ -0,0 +1,21 @@
+def highestprimefactor(a):
+    num = a
+    maxprime = 1
+    primelist = list()
+    i = 2
+    while i <= num:
+        isprime = True
+        for j in primelist:
+            if i % j == 0:
+                isprime = False
+                break
+        if isprime:
+            primelist.append(i)
+            while num % i == 0:
+                maxprime = i
+                num /= i
+        i += 1
+
+    return maxprime
+
+print(highestprimefactor(600851475143))
---
+++ README.md
@@ -0,0 +1,4 @@
+**Euler**
+
+These files are a couple algorithms I wrote to solve Project Euler problems. 
+I'm only beginning this now but I'd like to update it regularly ! 
