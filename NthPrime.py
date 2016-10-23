198cf7ac4a0aec869f635423dc57c7dfc4dda1dc
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-10-23 12:37:28 +0200

    Solve nth prime problem

---
+++ NthPrime.py
@@ -0,0 +1,16 @@
+def nth_prime(n):
+    prime_list = list()
+    i = 2
+    while len(prime_list) < n:
+        is_prime = True
+        for j in prime_list:
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_list.append(i)
+        i += 1
+
+    return prime_list[n-1]
+
+print(nth_prime(10001))
